2f42d15376a968db9448cb6d523a38e6ff95fcf1
gourav <gvirus21yt@gmail.com>
2022-04-27 10:54:36 +0530

    Added try catch for better error handling

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,11 +2,11 @@ import Nav from "../components/nav";
 import HomePage from "../components/home";
 import { useState, useEffect } from "react";
 const { ethers } = require("ethers");
-import ABI from './ABI.json'
+import ABI from "./ABI.json";
 
 export default function Home() {
   const [isConnected, setIsConnected] = useState(false);
-  const [balance, setBalance] = useState(0)
+  const [balance, setBalance] = useState(0);
   const [currentAccount, setCurrentAccount] = useState("");
   const [message, setMessage] = useState("");
   const [depositAmount, setDepositAmount] = useState(0);
@@ -15,18 +15,18 @@ export default function Home() {
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   let provider;
   let signer;
-  let contract
+  let contract;
 
   useEffect(() => {
     handleReload();
-    setup()
+    setup();
   }, []);
 
   const setup = () => {
     if (window.ethereum) {
       provider = new ethers.providers.Web3Provider(window.ethereum);
       signer = provider.getSigner();
-      contract = new ethers.Contract(contractAddress, ABI, signer)
+      contract = new ethers.Contract(contractAddress, ABI, signer);
     }
   };
 
@@ -38,24 +38,33 @@ export default function Home() {
       if (accounts[0] != undefined) {
         setCurrentAccount(accounts[0]);
         setIsConnected(true);
-        getBalance()
-        getMessage()
+        getBalance();
+        getMessage();
       }
     }
   };
 
   const getBalance = async () => {
-    const balance = await provider.getBalance(contractAddress);
-    const balanceFormatted = ethers.utils.formatEther(balance);
-    console.log(balanceFormatted)
-    setBalance(balanceFormatted)
+    try {
+      const balance = await provider.getBalance(contractAddress);
+      const balanceFormatted = ethers.utils.formatEther(balance);
+      console.log(balanceFormatted);
+      setBalance(balanceFormatted);
+    } catch (err) {
+      console.log(err);
+    }
+    
   };
 
   const getMessage = async () => {
-    console.log("get message called")
-    const contractMessage = await contract.message();
-    setMessage(contractMessage)
-  }
+    console.log("get message called");
+    try {
+      const contractMessage = await contract.message();
+      setMessage(contractMessage);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div>
